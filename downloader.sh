c406fb44ffcb9072e74f912b6ca0fc3488111656
Ceban Dumitru <cebandima@ymail.com>
2024-01-21 10:33:18 +0200

    chore: add error handle

--- downloader.sh
+++ downloader.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo -n "Please insert an Toonly URL:"
-read URL
+read -r URL
 
-toon=$(curl -s $URL)
+toon=$(curl -s "$URL")
 
 Title_Line=$(echo "$toon" | grep -E "^<title>")
 start_idx=$(expr length "<title>Read ")
@@ -11,23 +11,23 @@ end_idx=$(expr length " Manga - Toonily</title>")
 title=${Title_Line:$start_idx:-$end_idx}
 
 mkdir "$title"
-cd "$title"
+cd "$title" || exit
 
 chapters=$(echo "$toon" | grep -A1 -w "class=\"wp-manga-chapter" | grep href | cut -d "\"" -f2)
 for chapter in $chapters;
 do 
 	echo "Downloading $chapter"
-	chapter_dir=$(echo $chapter | cut -d "/" -f6)
+	chapter_dir=$(echo "$chapter" | cut -d "/" -f6)
 	
 	echo "Make directory $chapter_dir"
-	mkdir $chapter_dir
-	cd $chapter_dir
+	mkdir "$chapter_dir"
+	cd "$chapter_dir" || exit
 	
-	imgs=$(curl -s $chapter| grep -A1 -E "image-[[:digit:]]" | grep cdn | awk '{print substr($1,1,length($1)-1)}')
+	imgs=$(curl -s "$chapter"| grep -A1 -E "image-[[:digit:]]" | grep cdn | awk '{print substr($1,1,length($1)-1)}')
 	for img in $imgs;
 	do
 		echo "Saving $img"
-		curl -s $img -H 'authority: cdn.toonily.com' -H 'referer: https://toonily.com/' -O
+		curl -s "$img" -H 'authority: cdn.toonily.com' -H 'referer: https://toonily.com/' -O
 
 	done
 
