2da803a9e6d99a8c869424621d13c85ee7e30489
Ceban Dumitru <cebandima@ymail.com>
2024-03-02 19:06:09 +0200

    chore: exclude BSD from releases

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 LastTag=$(git tag -l --sort=-version:refname "v*" | head -n 1)
-OS=("openbsd" "freebsd" "darwin" "linux" "windows")
+OS=("darwin" "linux" "windows")
 ARCH=("amd64" "386")
 
-for os in ${OS[@]}
+for os in "${OS[@]}"
 do
 	echo "Building $os..."
-	for arch in ${ARCH[@]}
+	for arch in "${ARCH[@]}"
 	do
 		echo "+$arch"
 		if [ "$os" = "windows" ]; then
